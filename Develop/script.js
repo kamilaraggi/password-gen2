48956793e5d84c91fba8a1b9ad3ac4752162c8e5
Kamila Raggi <kamilaraggi@hotmail.com>
2021-08-18 21:06:32 -0400

    updates

--- Develop/script.js
+++ Develop/script.js
@@ -35,33 +35,29 @@ console.log("password length " + passwordLength);
 
 if (!passwordLength){
   alert("required value");
-} 
-
-
-else if (passwordLength<8 || passwordLength >128) {
+} else if (passwordLength<8 || passwordLength >128) {
   passwordLength = window.prompt ("Choose a number between 8 - 128");
-}
 
-else{
+} else{
  
   var confirmLowerCase = window.confirm("Password must include Lower Case characters.")
   console.log("lower case " + confirmLowerCase);
 
   var confirmUpperCase = window.confirm("Password must include Upper Case characters.")
-  console.log("upper case" + confirmUpperCase);
+  console.log("upper case " + confirmUpperCase);
 
   var confirmNumber = window.confirm("Password must include Number characters.")
-  console.log("number" + confirmNumber);
+  console.log("number " + confirmNumber);
 
   var confirmSymbol = window.confirm("Password must include Symbol characters.")
-  console.log("symbol" + confirmSymbol);
+  console.log("symbol " + confirmSymbol);
 
 if (!confirmLowerCase && !confirmUpperCase && !confirmNumber && !confirmSymbol){
   userChoices = alert(" Please select one! "); 
 }}
 
-var password = "";
-
+let password = '';
+let userChoices = [];
 
 if (confirmLowerCase) password += lowerCase;
 
@@ -71,11 +67,13 @@ if (confirmNumber) password += number;
 
 if (confirmSymbol) password += symbol;
 
-let returnPassword = '';
+const allChoices = []
+var returnPassword = '';
+
 for (var i = 0; i < passwordLength; i++) {
-  returnPassword = returnPassword+password[Math.floor(Math.random * password.length)]
+  returnPassword = returnPassword+password[Math.floor(Math.random() * password.length)]
 }
-console.log(password + returnPassword);
+
 
 return returnPassword;
 
